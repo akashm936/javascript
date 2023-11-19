7e39fa3c529f449c486f04f118cbd18919da70b4
Akash Morwal <akashbhau101@gmail.com>
2023-11-19 07:59:48 +0530

    learn datatype done

---
+++ basics_js/datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; //this used in modern javascript for handling error
+
+//alert( 3 + 3) // not use in node.js
+
+// console.log( 3 
+//     + 3); //end the readability of documnet not use this type of syntax
+    
+console.log( "Akash");
+
+// Datatype of Javascript
+
+let name = "Akash" //string
+let age = 22   // number
+let result = true //boolean
+let state;  //undefined
+
+
+// string  => ""
+// number  => 2112
+//boolean  => true/false
+//null => // standalone Value 
+//undefind  
+//bigInt  //big value
+//symbol => unique
+
+//object
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof undefined);
+console.log(typeof null) //it return the object in type 
+
---
+++ basics_js/variables.js
@@ -0,0 +1,12 @@
+const empid = 12345
+let empname = "Akash"
+var empadd = "nutan Vasahat"
+phoneno = 8917163143
+let empp
+
+
+// var is not support modern javascript
+
+console.log(empid); // for print one value in console
+
+console.table([empid, empname, empadd, phoneno, empp]); //for printing multiple value at same time in table
