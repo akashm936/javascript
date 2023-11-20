fc675f9c0efb0c284aff5f3ef9f7c81627f93121
Akash Morwal <akashbhau101@gmail.com>
2023-11-20 23:26:43 +0530

    operation done

--- basics_js/conversion.js
+++ basics_js/conversion.js
@@ -15,8 +15,8 @@ let convertintoString = String(number) //for convert into String value
 
 let bol = 1
 let convertintoboolean = Boolean(bol) //for convert into boolean value
-console.log(convertintoboolean);
-console.log(typeof convertintoboolean);  
+// console.log(convertintoboolean);
+// console.log(typeof convertintoboolean);  
 
 // "" => false
 // "Akash" => true
@@ -24,3 +24,34 @@ console.log(typeof convertintoboolean);
 // 0 => false
 
 //NaN is Not a Number
+
+// ****************************JAVASCRIPT OPERATION ***********************
+
+
+let a = 2
+let b = 3
+let c = 5
+let d = a + b * c
+
+// console.log(d);
+
+// console.log(1 + 1);
+// console.log(2 - 2);
+// console.log(2**2);
+// console.log(3%3);
+// console.log( 2 + 2 - 3 * 5 % 6) //this not a right way to write a code 
+
+// console.log("1" + 2)
+// console.log(2 + "1")
+// console.log(2 + 2 + "1"); //javascript perform 1 operation on full series string or numbers
+
+// this all operation depend on the prefix or postfix 
+
+let score1 = 100
+score1++;  //postfix  /  this is right way to write a code
+++score1;  // prefix
+console.log(score1);
+
+
+
+
