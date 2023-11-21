0be372c6b8579e137014f749d1d2855237a38df1
Akash Morwal <akashbhau101@gmail.com>
2023-11-21 08:00:26 +0530

    datatype comparison done

---
+++ basics_js/datatype_comparison.js
@@ -0,0 +1,33 @@
+// console.log(2 > 1)
+// console.log(3 >= 1);
+// console.log(3 < 0);
+// console.log(3 != 2);
+// console.log( 2 == 2);
+
+//comapare with datatype
+
+// console.log("2" > 1);
+// console.log("1" <= 2);
+// console.log(3 == "2");
+
+
+//compare with null
+
+// console.log(null > 1);
+// console.log(null <= 0);
+// console.log(null >= 0);
+
+
+//compare with undefind
+
+// console.log(undefined > 1);
+// console.log(undefined >= 1);
+// console.log(undefined == 1);
+
+//compare with === its check value with datatypes
+
+console.log("1" === 2);
+console.log(1 === 1);
+console.log("2" === "2");
+console.log( "2" > 3);
+console.log("3" > 1);
